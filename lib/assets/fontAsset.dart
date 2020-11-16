fba3e153a48e69ade532b453c276cc4bfd434b32
David <davidadegoke16@gmail.com>
2020-11-16 01:48:16 +0100

    changed title fontSize to 18

--- lib/assets/fontAsset.dart
+++ lib/assets/fontAsset.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 class FontAsset {
-  static final TextStyle title = TextStyle(
-    fontSize: 20,
-    color: Colors.white,
-  );
+  static final TextStyle title = TextStyle(fontSize: 18);
+  static final TextStyle welcomeTitle = TextStyle(fontSize: 25);
   static final TextStyle regularText = TextStyle(fontSize: 18);
   static final TextStyle unknownFont = TextStyle();
 }
